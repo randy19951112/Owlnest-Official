a09082a7c3eceb2682b2b3d17a03fa9c4614fd6b
randy19951112 <s97140400@gmail.com>
2026-01-26 10:33:41 +0800

    Delete netlify/account/js directory

--- netlify/account/js/supabaseClient.js
+++
@@ -1,6 +0,0 @@
-import { createClient } from "https://esm.sh/@supabase/supabase-js@2";
-
-export const supabase = createClient(
-  "https://khoiplqugajmybmultzs.supabase.co",
-  "sb_publishable_ic3b9TeYt7SuXxLIhLuyvA_FWHYVb0Z" // ✅ 用 sb_publishable / anon key，前端不要用 service role
-);
