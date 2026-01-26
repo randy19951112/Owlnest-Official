ceed55c055b8469d57cb8da5ebfbe145d397e442
randy19951112 <s97140400@gmail.com>
2026-01-26 10:36:34 +0800

    Create supabaseClient.js

---
+++ account/js/supabaseClient.js
@@ -0,0 +1,6 @@
+import { createClient } from "https://esm.sh/@supabase/supabase-js@2";
+
+export const supabase = createClient(
+  "https://khoiplqugajmybmultzs.supabase.co",
+  "sb_publishable_ic3b9TeYt7SuXxLIhLuyvA_FWHYVb0Z" // sb_publishable_...
+);
