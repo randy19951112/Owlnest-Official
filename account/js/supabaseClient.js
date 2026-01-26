d2c0ef9d6923f999b1548a693737af631c77f45a
randy19951112 <s97140400@gmail.com>
2026-01-26 12:06:34 +0800

    Update supabaseClient.js

--- account/js/supabaseClient.js
+++ account/js/supabaseClient.js
@@ -2,5 +2,5 @@ import { createClient } from "https://esm.sh/@supabase/supabase-js@2";
 
 export const supabase = createClient(
   "https://khoiplqugajmybmultzs.supabase.co",
-  "sb_publishable_ic3b9TeYt7SuXxLIhLuyvA_FWHYVb0Z" // sb_publishable_...
+  "sb_publishable_ic3b9TeYt7SuXxLIhLuyvA_FWHYVb0Z" // 請確認這組 Key 與您 member-login.html 內使用的是同一組
 );
